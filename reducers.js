23067ad6fe1a459355a2092a77a2f816f7da190e
benleichter <bleichter@bevspot.com>
2016-11-27 23:47:05 -0500

    fixed copy

--- reducers.js
+++ reducers.js
@@ -12,7 +12,7 @@ const initialState = {
             datesOfEmployment: 'October 2015 - Current',
             url: 'https://www.bevspot.com',
             projects: [],
-            description: 'At BevSpot, I work on a web app for bars and restaurants to manage their inventory, order from distributors, and track their sales data. We get a great deal of freedom to try new technologies as long as we can give sound reasons for how they might make our code better or improve the development process. As a result, I\'ve been able to start writing React apps and begin using TypeScript within the time that I\'ve been working here. In addition to working with interesting front-end technologies, I get to be involved in a lot of data design sessions and write a satisfying amount of Python and Java.',
+            description: 'At BevSpot, I work on a web app for bars and restaurants to manage their inventory, order from distributors, and track their sales data. We get a great deal of freedom to try new technologies as long as we can give sound reasons for how they might make our code better or improve the development process. As a result, I\'ve been able to start writing React apps and begin using TypeScript. In addition to working with interesting front-end technologies, I get to be involved in a lot of data design sessions and write a good amount of Python and Java.',
             relevantSkills: ['JavaScript', 'ReactJS', 'TypeScript', 'Django', 'Java']
         },
         'mtc': {
@@ -24,7 +24,7 @@ const initialState = {
                 name: 'Vocoli',
                 url: 'https://www.vocoli.com',
             }],
-            description: 'At MTC, I mostly worked on an employee engagement software called Vocoli. The jist of Vocoli is to empower employees through anonymous suggestions and polls, and to give management the tools to create surveys and gather informations from the data collected by them. I would receive a mock from our designer and build whole features starting with the UI, all the way through to the database.',
+            description: 'At MTC, I mostly worked on an employee engagement software called Vocoli. The jist of Vocoli is to empower employees through anonymous suggestions and polls, and to give management the tools to create surveys and gather information from the data collected by them. I would receive a mock from our designer and build whole features, from the UI, all the way through to the database.',
             relevantSkills: ['HTML', 'CSS', 'JavaScript', 'PHP', 'SQL']
         },
         'harvard-cid': {
@@ -36,7 +36,7 @@ const initialState = {
                 name: 'Atlas of Economic Complexity',
                 url: 'http://atlas.cid.harvard.edu/',
             }],
-            description: 'At CID, I worked on the front-end of a tool that visualizes the economic makeup of countries. The idea is to help nations make connections between products they make with the resources they have, and products they could be making with those same resources. They can also look at what countries their neighbors have been trading with and what they\'ve been trading. In this way, they can potentially find new partners with minimal effort.',
+            description: 'At CID, I worked on the front-end of a tool that visualizes the economic makeup of countries. The idea is to help countires make connections between products they make with the resources they have, and products they could be making with those same resources. They can also look at what countries their neighbors have been trading with and what they\'ve been trading. In this way, they can potentially find new trade partners with minimal effort.',
             relevantSkills: ['HTML', 'CSS', 'JavaScript', 'd3'],
         },
     }
