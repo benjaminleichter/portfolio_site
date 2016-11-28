f3f935a2d3f7ed50a03bdf9d38cc8427791923d0
benleichter <bleichter@bevspot.com>
2016-11-28 12:30:40 -0500

    styling changes

--- components/AboutMe.jsx
+++ components/AboutMe.jsx
@@ -44,9 +44,9 @@ class AboutMe extends React.Component {
                             <div className="under-line-inner" />
                         </div>
                         <span className="footer-text">
-                            Check out the code for this website on <a href="https://github.com/benjaminleichter/portfolio_site" target="_">GitHub</a>.
+                            Check out the code for this website on <a className="hover-me" href="https://github.com/benjaminleichter/portfolio_site" target="_">GitHub</a>.
                             <br />
-                            Icons on this site created by <a href="https://thenounproject.com/ryo.sato.7127/" target="_">Ryo Sato</a> and used under the <a href="https://creativecommons.org/" target="_">Creative Commons License</a>.
+                            Icons on this site created by <a className="hover-me" href="https://thenounproject.com/ryo.sato.7127/" target="_">Ryo Sato</a> and used under the <a className="hover-me" href="https://creativecommons.org/" target="_">Creative Commons License</a>.
                         </span>
                     </div>
                 </div>
